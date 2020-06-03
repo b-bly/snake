4df8c23f9cc914e6913e009d4d8afdcefab20639
Brendt Bly <brendt.bly@outlook.com>
2020-06-03 15:53:18 -0500

    Updates

---
+++ precache-manifest.69ac69227ab126da9be9fa887e774420.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea98300e653350c3a5d71dd5a49b5b77",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "5e6283a26a0a1476c49a",
+    "url": "/snake/static/css/main.6252178d.chunk.css"
+  },
+  {
+    "revision": "1e2328a3a8fc425ca616",
+    "url": "/snake/static/js/2.8dcce4ec.chunk.js"
+  },
+  {
+    "revision": "c1f9a2eb041d4688a3bf0bf88144705a",
+    "url": "/snake/static/js/2.8dcce4ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e6283a26a0a1476c49a",
+    "url": "/snake/static/js/main.aa4075d4.chunk.js"
+  },
+  {
+    "revision": "8e9626a4e2336e41e995",
+    "url": "/snake/static/js/runtime-main.75f7c670.js"
+  }
+]);
